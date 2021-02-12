98e9b10964b0544390dc9e842af426d9815d6fc6
defiekosjo <dkosho595@gmail.com>
2021-02-12 17:06:20 +0100

    ecosystem adjusted

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -10,37 +10,69 @@
           icon="menu"
           aria-label="Menu"
         />
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-        <q-space/>
+        <q-toolbar-title> Gibas Ecosystem </q-toolbar-title>
+        <q-space />
         <div class="q-gutter-sm row items-center no-wrap">
-          <q-btn round dense flat color="white" :icon="$q.fullscreen.isActive ? 'fullscreen_exit' : 'fullscreen'"
-                 @click="$q.fullscreen.toggle()"
-                 v-if="$q.screen.gt.sm">
+          <q-btn
+            round
+            dense
+            flat
+            color="white"
+            :icon="$q.fullscreen.isActive ? 'fullscreen_exit' : 'fullscreen'"
+            @click="$q.fullscreen.toggle()"
+            v-if="$q.screen.gt.sm"
+          >
           </q-btn>
-          <q-btn round dense flat color="white" icon="fab fa-github" type="a" href="https://github.com/pratik227/quasar-admin" target="_blank">
+          <!-- <q-btn
+            round
+            dense
+            flat
+            color="white"
+            icon="fab fa-github"
+            type="a"
+            href="https://github.com/"
+            target="_blank"
+          >
+          </q-btn> -->
+          <q-btn
+            round
+            dense
+            flat
+            icon="fas fa-heart"
+            style="color: #9d4182 !important"
+            type="a"
+            href="https://gibas.nl"
+            target="_blank"
+          >
           </q-btn>
-          <q-btn round dense flat icon="fas fa-heart" style="color:#9d4182 !important;" type="a" href="https://github.com/sponsors/pratik227" target="_blank">
-          </q-btn>
+          <q-btn
+            flat
+            round
+            @click="$q.dark.toggle()"
+            :icon="$q.dark.isActive ? 'nights_stay' : 'wb_sunny'"
+          />
+          <!-- code of line to setup dark mode -->
+          <!-- <q-toolbar :class="$q.dark.isActive ? 'bg-warning' : 'bg-primary'" /> -->
           <q-btn round dense flat color="white" icon="notifications">
-            <q-badge color="red" text-color="white" floating>
-              5
-            </q-badge>
-            <q-menu
-            >
+            <q-badge color="red" text-color="white" floating> 5 </q-badge>
+            <q-menu>
               <q-list style="min-width: 100px">
                 <messages></messages>
                 <q-card class="text-center no-shadow no-border">
-                  <q-btn label="View All" style="max-width: 120px !important;" flat dense
-                         class="text-indigo-8"></q-btn>
+                  <q-btn
+                    label="View All"
+                    style="max-width: 120px !important"
+                    flat
+                    dense
+                    class="text-indigo-8"
+                  ></q-btn>
                 </q-card>
               </q-list>
             </q-menu>
           </q-btn>
           <q-btn round flat>
             <q-avatar size="26px">
-              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
             </q-avatar>
           </q-btn>
         </div>
@@ -53,200 +85,219 @@
       bordered
       content-class="bg-primary text-white"
     >
-      <q-list>
-        <q-item to="/" active-class="q-item-no-link-highlighting">
-          <q-item-section avatar>
-            <q-icon name="dashboard"/>
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Dashboard</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item to="/Dashboard2" active-class="q-item-no-link-highlighting">
-          <q-item-section avatar>
-            <q-icon name="dashboard"/>
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>CRM Dashboard</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-expansion-item
-          icon="pages"
-          label="Pages"
-        >
-          <q-list class="q-pl-lg">
-            <q-item to="/Login-1" active-class="q-item-no-link-highlighting">
+      <q-scroll-area style="height: 100%; max-width: 100%">
+        <q-list>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr1XdHERcHd2OyUVkB2T3FQ93cUycrUFT-eg&usqp=CAU"
+            style="width: 100%"
+          />
+          <q-item to="/" active-class="q-item-no-link-highlighting">
+            <q-item-section avatar>
+              <q-icon name="dashboard" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Dashboard</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <!-- <q-item to="/Dashboard2" active-class="q-item-no-link-highlighting">
+            <q-item-section avatar>
+              <q-icon name="dashboard" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>CRM Dashboard</q-item-label>
+            </q-item-section>
+          </q-item> -->
+
+          <q-expansion-item icon="pages" label="User Settings">
+            <q-list class="q-pl-lg">
+              <!-- <q-item to="/Lock" active-class="q-item-no-link-highlighting">
               <q-item-section avatar>
-                <q-icon name="email"/>
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Login-1</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item to="/Lock" active-class="q-item-no-link-highlighting">
-              <q-item-section avatar>
-                <q-icon name="lock"/>
+                <q-icon name="lock" />
               </q-item-section>
               <q-item-section>
                 <q-item-label>Lock Screen</q-item-label>
               </q-item-section>
-            </q-item>
-            <q-item to="/Lock-2" active-class="q-item-no-link-highlighting">
+            </q-item> -->
+              <q-item-label header class="text-weight-bolder text-white"
+                >User Settings</q-item-label
+              >
+
+              <q-item to="/Login-1" active-class="q-item-no-link-highlighting">
+                <q-item-section avatar>
+                  <q-icon name="email" />
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label>Login-1</q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item to="/Lock-2" active-class="q-item-no-link-highlighting">
+                <q-item-section avatar>
+                  <q-icon name="lock" />
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label>Lock Screen - 2</q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item to="/Profile" active-class="q-item-no-link-highlighting">
+                <q-item-section avatar>
+                  <q-icon name="person" />
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label>User Profile</q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item to="/Contact" active-class="q-item-no-link-highlighting">
+                <q-item-section avatar>
+                  <q-icon name="person" />
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label>Contact</q-item-label>
+                </q-item-section>
+              </q-item>
+              <!-- <q-item to="/Pricing" active-class="q-item-no-link-highlighting">
               <q-item-section avatar>
-                <q-icon name="lock"/>
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Lock Screen - 2</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item to="/Pricing" active-class="q-item-no-link-highlighting">
-              <q-item-section avatar>
-                <q-icon name="list"/>
+                <q-icon name="list" />
               </q-item-section>
               <q-item-section>
                 <q-item-label>Pricing</q-item-label>
               </q-item-section>
-            </q-item>
-            <q-item-label header class="text-weight-bolder text-white">Generic</q-item-label>
-            <q-item to="/Profile" active-class="q-item-no-link-highlighting">
+            </q-item> -->
+
+              <!-- <q-item
+              to="/Maintenance"
+              active-class="q-item-no-link-highlighting"
+            >
               <q-item-section avatar>
-                <q-icon name="person"/>
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>User Profile</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item to="/Maintenance" active-class="q-item-no-link-highlighting">
-              <q-item-section avatar>
-                <q-icon name="settings"/>
+                <q-icon name="settings" />
               </q-item-section>
               <q-item-section>
                 <q-item-label>Maintenance</q-item-label>
               </q-item-section>
-            </q-item>
-          </q-list>
-        </q-expansion-item>
-        <q-expansion-item
-          icon="map"
-          label="Maps"
-        >
-          <q-list class="q-pl-lg">
-            <q-item to="/Map" active-class="q-item-no-link-highlighting">
-              <q-item-section avatar>
-                <q-icon name="map"/>
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Map</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item to="/MapMarker" active-class="q-item-no-link-highlighting">
-              <q-item-section avatar>
-                <q-icon name="location_on"/>
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Map Marker</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item to="/StreetView" active-class="q-item-no-link-highlighting">
-              <q-item-section avatar>
-                <q-icon name="streetview"/>
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Street View</q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </q-expansion-item>
+            </q-item> -->
+            </q-list>
+          </q-expansion-item>
+          <q-expansion-item icon="map" label="Maps">
+            <q-list class="q-pl-lg">
+              <q-item to="/Map" active-class="q-item-no-link-highlighting">
+                <q-item-section avatar>
+                  <q-icon name="map" />
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label>Map</q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item
+                to="/MapMarker"
+                active-class="q-item-no-link-highlighting"
+              >
+                <q-item-section avatar>
+                  <q-icon name="location_on" />
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label>Map Marker</q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item
+                to="/StreetView"
+                active-class="q-item-no-link-highlighting"
+              >
+                <q-item-section avatar>
+                  <q-icon name="streetview" />
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label>Street View</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-expansion-item>
 
-        <q-item to="/Mail" active-class="q-item-no-link-highlighting">
+          <!-- <q-item to="/Mail" active-class="q-item-no-link-highlighting">
           <q-item-section avatar>
             <q-icon name="email"/>
           </q-item-section>
           <q-item-section>
             <q-item-label>Mail</q-item-label>
           </q-item-section>
-        </q-item>
-        <q-item to="/TreeTable" active-class="q-item-no-link-highlighting">
-          <q-item-section avatar>
-            <q-icon name="list"/>
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>TreeTable</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item to="/Charts" active-class="q-item-no-link-highlighting">
-          <q-item-section avatar>
-            <q-icon name="insert_chart"/>
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Charts</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item to="/Cards" active-class="q-item-no-link-highlighting">
-          <q-item-section avatar>
-            <q-icon name="card_giftcard"/>
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Cards</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item to="/Tables" active-class="q-item-no-link-highlighting">
-          <q-item-section avatar>
-            <q-icon name="table_chart"/>
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Tables</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item to="/Contact" active-class="q-item-no-link-highlighting">
-          <q-item-section avatar>
-            <q-icon name="person"/>
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Contact</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item to="/Checkout" active-class="q-item-no-link-highlighting">
+        </q-item> -->
+
+          <q-item to="/TreeTable" active-class="q-item-no-link-highlighting">
+            <q-item-section avatar>
+              <q-icon name="list" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>TreeTable</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item to="/Charts" active-class="q-item-no-link-highlighting">
+            <q-item-section avatar>
+              <q-icon name="insert_chart" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Charts</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item to="/Cards" active-class="q-item-no-link-highlighting">
+            <q-item-section avatar>
+              <q-icon name="card_giftcard" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Cards</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item to="/Tables" active-class="q-item-no-link-highlighting">
+            <q-item-section avatar>
+              <q-icon name="table_chart" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Tables</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <!-- <q-item to="/Checkout" active-class="q-item-no-link-highlighting">
           <q-item-section avatar>
             <q-icon name="check_circle_outline"/>
           </q-item-section>
           <q-item-section>
             <q-item-label>Checkout</q-item-label>
           </q-item-section>
-        </q-item>
-        <q-item to="/Calendar" active-class="q-item-no-link-highlighting">
+        </q-item> -->
+
+          <!-- <q-item to="/Calendar" active-class="q-item-no-link-highlighting">
           <q-item-section avatar>
             <q-icon name="date_range"/>
           </q-item-section>
           <q-item-section>
             <q-item-label>Calendar</q-item-label>
           </q-item-section>
-        </q-item>
-        <q-item to="/Taskboard" active-class="q-item-no-link-highlighting">
-          <q-item-section avatar>
-            <q-icon name="done"/>
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Taskboard</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item to="/Pagination" active-class="q-item-no-link-highlighting">
-          <q-item-section avatar>
-            <q-icon name="date_range"/>
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Pagination</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item to="/Ecommerce" active-class="q-item-no-link-highlighting">
+        </q-item> -->
+          <q-item to="/Taskboard" active-class="q-item-no-link-highlighting">
+            <q-item-section avatar>
+              <q-icon name="done" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Taskboard</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item to="/Pagination" active-class="q-item-no-link-highlighting">
+            <q-item-section avatar>
+              <q-icon name="date_range" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Pagination</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <!-- <q-item to="/Ecommerce" active-class="q-item-no-link-highlighting">
           <q-item-section avatar>
             <q-icon name="shopping_cart"/>
           </q-item-section>
           <q-item-section>
             <q-item-label>Product Catalogues</q-item-label>
           </q-item-section>
-        </q-item>
-        <q-expansion-item
+        </q-item> -->
+
+          <!-- <q-expansion-item
           icon="menu_open"
           label="Menu Levels"
         >
@@ -280,32 +331,33 @@
               </q-item>
             </q-expansion-item>
           </q-expansion-item>
-        </q-expansion-item>
-      </q-list>
+        </q-expansion-item> -->
+        </q-list>
+      </q-scroll-area>
     </q-drawer>
 
     <q-page-container class="bg-grey-2">
-      <router-view/>
+      <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-    import EssentialLink from 'components/EssentialLink'
-    import Messages from "./Messages";
+import EssentialLink from "components/EssentialLink";
+import Messages from "./Messages";
 
-    export default {
-        name: 'MainLayout',
+export default {
+  name: "MainLayout",
 
-        components: {
-            Messages,
-            EssentialLink
-        },
+  components: {
+    Messages,
+    EssentialLink,
+  },
 
-        data() {
-            return {
-                leftDrawerOpen: false,
-            }
-        }
-    }
+  data() {
+    return {
+      leftDrawerOpen: false,
+    };
+  },
+};
 </script>
